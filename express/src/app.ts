37d258cdd08b8ee236934f1655ae717c6736611c
Wasim Hossain <wasim.hossain003@gmail.com>
2025-06-05 05:11:43 +0600

    ...

---
+++ express/src/app.ts
@@ -0,0 +1,11 @@
+import express, { Application, Request, Response } from 'express'
+const app : Application = express()
+const port = 3000
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
